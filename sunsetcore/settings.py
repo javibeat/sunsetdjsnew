f56fc71c2563aad46437c1fedff50c7a82232c01
javibeat <javibeat@gmail.com>
2025-05-22 12:18:12 +0400

    Back to Railway: stable config and DB fix

--- sunsetcore/settings.py
+++ sunsetcore/settings.py
@@ -84,19 +84,13 @@ WSGI_APPLICATION = 'sunsetcore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 
 # Password validation
